777431394fd2601c54fea191f446a2a6f01c360a
azhadsyed <azhadsyed17@gmail.com>
2025-01-03 00:29:10 -0500

    first nice chop

--- bags.js
+++ bags.js
@@ -1,3 +1,6 @@
 samples('github:azhadsyed/strudel-samples');
   
-s("bags:1").loopAt(32).slice(64, "<[3!6 2!10]!2 [4!7 5!4 5.25!5] [6!8 7!4 7.25!4]>").slow(2).cut(1)
+setcps(160/60/4)
+
+// TODO add stutter bass effect
+s("bags:1").loopAt(32).slice(64, "<[3!6 2!6 2.25!4]!2 [4!7 5!4 5.25!5] [6!9 7!4 7.25!3]>").slow(2).cut(1).speed(0.01)
